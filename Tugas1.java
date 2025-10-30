6606dcbb58510c1a20af3821ee0c43c9aa62ffd5
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-10-30 22:50:49 +0700

    input nilai N

---
+++ Tugas1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N : ");
+        int n = sc.nextInt();
+    }
+}
