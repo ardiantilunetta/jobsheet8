22940d16d35a691a4e817d12a5de930d4e59f01b
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-10-30 22:54:27 +0700

    kondisi cetak *

--- Tugas1.java
+++ Tugas1.java
@@ -7,8 +7,11 @@ public class Tugas1 {
         int n = sc.nextInt();
         for (int i = 1 ; i <= n ; i++) {
             for (int j = 1 ; j <= n ; j++) {
-                
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                }
             }
+            System.out.println();
         }
     }
 }
