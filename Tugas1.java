377e51f9060cc762bd79e9aac45240d2a3860632
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-10-30 22:52:43 +0700

    inner loop

--- Tugas1.java
+++ Tugas1.java
@@ -6,7 +6,9 @@ public class Tugas1 {
         System.out.print("Masukkan nilai N : ");
         int n = sc.nextInt();
         for (int i = 1 ; i <= n ; i++) {
-            
+            for (int j = 1 ; j <= n ; j++) {
+                
+            }
         }
     }
 }
