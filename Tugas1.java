8c23586663d06f211ec77b55f2e0c55f28efddb1
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-10-30 22:56:11 +0700

    kondisi cetak " "

--- Tugas1.java
+++ Tugas1.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Tugas1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan nilai N : ");
+        System.out.print("Masukkan nilai N (minimal 3) : ");
         int n = sc.nextInt();
         for (int i = 1 ; i <= n ; i++) {
             for (int j = 1 ; j <= n ; j++) {
                 if (i == 1 || i == n || j == 1 || j == n) {
                     System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
