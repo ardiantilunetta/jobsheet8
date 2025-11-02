b2a9c9d1c9a24bff6b4507053d796c4f478e205e
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-02 21:09:52 +0700

    output benar

--- Tugas2.java
+++ Tugas2.java
@@ -11,36 +11,41 @@ public class Tugas2 {
         sc.nextLine();
 
         for (int i=1 ; i<=n ; i++){
+
+            System.out.println("POLITEKNIK "+i);
+
             System.out.println("Masukkan nama atlet cabor Badminton : ");
             for (int j=1 ; j<=5 ; j++) {
                 System.out.print(j+". ");
-                badminton += sc.nextLine();
+                badminton += sc.nextLine() + ", ";
             }
 
             System.out.println("Masukkan nama atlet cabor tenis meja : ");
             for (int k=1 ; k<=5 ; k++) {
                 System.out.print(k+". ");
-                tenis += sc.nextLine();
+                tenis += sc.nextLine() + ", ";
             }
 
             System.out.println("Masukkan nama atlet cabor basket : ");
             for (int l=1 ; l<=5 ; l++) {
                 System.out.print(l+". ");
-                basket += sc.nextLine();
+                basket += sc.nextLine() + ", ";
             }
 
             System.out.println("Masukkan nama atlet cabor voly : ");
             for (int m=1 ; m<=5 ; m++) {
                 System.out.print(m+". ");
-                voly += sc.nextLine();
+                voly += sc.nextLine() + ", ";
             }
+
+            System.out.println();
         }
 
         System.out.println("----- DAFTAR ATLET PORSENI 2024 -----");
         System.out.println("Jumlah politeknik : "+n);
-        System.out.println("Cabor Badminton : "+badminton);
-        System.out.println("Cabor Tenis Meja : "+tenis);
-        System.out.println("Cabor Basket : "+basket);
-        System.out.println("Cabor Voly : "+voly);        
+        System.out.println("Cabor Badminton   : "+badminton);
+        System.out.println("Cabor Tenis Meja  : "+tenis);
+        System.out.println("Cabor Basket      : "+basket);
+        System.out.println("Cabor Voly        : "+voly);        
     }
 }
