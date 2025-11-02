e6c6c795cf143a265fbad81b056715a7b34e5405
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-02 20:56:45 +0700

    cetak daftar atlet

--- Tugas2.java
+++ Tugas2.java
@@ -11,35 +11,36 @@ public class Tugas2 {
         sc.nextLine();
 
         for (int i=1 ; i<=n ; i++){
-            System.out.println("Masukkan nama pemain cabor Badminton : ");
+            System.out.println("Masukkan nama atlet cabor Badminton : ");
             for (int j=1 ; j<=5 ; j++) {
                 System.out.print(j+". ");
                 badminton += sc.nextLine();
             }
 
-            System.out.print("Masukkan nama pemain cabor tenis meja : ");
+            System.out.println("Masukkan nama atlet cabor tenis meja : ");
             for (int k=1 ; k<=5 ; k++) {
                 System.out.print(k+". ");
                 tenis += sc.nextLine();
             }
 
-            System.out.print("Masukkan nama pemain cabor basket : ");
+            System.out.println("Masukkan nama atlet cabor basket : ");
             for (int l=1 ; l<=5 ; l++) {
                 System.out.print(l+". ");
                 basket += sc.nextLine();
             }
 
-            System.out.print("Masukkan nama pemain cabor voly : ");
+            System.out.println("Masukkan nama atlet cabor voly : ");
             for (int m=1 ; m<=5 ; m++) {
                 System.out.print(m+". ");
                 voly += sc.nextLine();
             }
         }
 
-        System.out.println(badminton);
-        System.out.println(tenis);
-        System.out.println(basket);
-        System.out.println(voly);
-        
+        System.out.println("----- DAFTAR ATLET PORSENI 2024 -----");
+        System.out.println("Jumlah politeknik : "+n);
+        System.out.println("Cabor Badminton : "+badminton);
+        System.out.println("Cabor Tenis Meja : "+tenis);
+        System.out.println("Cabor Basket : "+basket);
+        System.out.println("Cabor Voly : "+voly);        
     }
 }
