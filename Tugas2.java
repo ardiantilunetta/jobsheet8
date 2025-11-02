cd9502d4c38db950fefa751f03b007601fc30a6c
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-11-02 20:50:02 +0700

    looping keempat cabor

--- Tugas2.java
+++ Tugas2.java
@@ -16,10 +16,30 @@ public class Tugas2 {
                 System.out.print(j+". ");
                 badminton += sc.nextLine();
             }
+
+            System.out.print("Masukkan nama pemain cabor tenis meja : ");
+            for (int k=1 ; k<=5 ; k++) {
+                System.out.print(k+". ");
+                tenis += sc.nextLine();
+            }
+
+            System.out.print("Masukkan nama pemain cabor basket : ");
+            for (int l=1 ; l<=5 ; l++) {
+                System.out.print(l+". ");
+                basket += sc.nextLine();
+            }
+
+            System.out.print("Masukkan nama pemain cabor voly : ");
+            for (int m=1 ; m<=5 ; m++) {
+                System.out.print(m+". ");
+                voly += sc.nextLine();
+            }
         }
 
         System.out.println(badminton);
-
+        System.out.println(tenis);
+        System.out.println(basket);
+        System.out.println(voly);
         
     }
 }
